ff39431756a6c0e09c80edd027a2a0124b77526b
Andrew Fessler <AF414@live.com>
2019-07-10 14:15:33 +0200

    Added variable task sizes and added cached queries (pickling)

--- FacilityLocation.py
+++ FacilityLocation.py
@@ -7,6 +7,8 @@ import json
 import requests
 from ortools.linear_solver import pywraplp
 import configparser
+import os
+import pickle
 
 # Account and API info
 config = configparser.ConfigParser()
@@ -25,29 +27,39 @@ def main():
     fac_number = [0, 1, 2, 3, 4]
     cus_number = [25, 26, 27, 28, 29, 30, 31, 32]
 
+    cached_file_name = 'cached_cost_'+str(fac_number).replace(",", "_")+str(cus_number).replace(",", "_")+'.pickle'
     cost = []
-    for fac in fac_number:
-        fact = []
-        for cus in cus_number:
-            print(city_data[fac]['city'], city_data[fac]['latitude'], city_data[fac]['longitude'])
-            print(city_data[cus]['city'], city_data[cus]['latitude'], city_data[cus]['longitude'])
-            cost_opt = ""  # &cost_optimize=1
 
-            request_string = here_api + "&waypoint0=" + str(city_data[fac]['latitude']) + "," + str(
-                city_data[fac]['longitude']) + "&waypoint1=" + str(city_data[cus]['latitude']) + "," + str(
-                city_data[cus]['longitude']) + "&mode=fastest;car" + cost_opt
-            r = requests.get(request_string)
+    if not os.path.exists(cached_file_name):
+        for fac in fac_number:
+            fact = []
+            for cus in cus_number:
+                print(city_data[fac]['city'], city_data[fac]['latitude'], city_data[fac]['longitude'])
+                print(city_data[cus]['city'], city_data[cus]['latitude'], city_data[cus]['longitude'])
+                cost_opt = ""  # &cost_optimize=1
 
-            print(r.status_code)
-            # print(r.text)
-            here_data = r.json()
-            try:
-                here_cost = float(here_data['costs']['totalCost'])
-            except:
-                here_cost = "NA"
-            fact.append(here_cost)
-        cost.append(fact)
-        print(fact)
+                request_string = here_api + "&waypoint0=" + str(city_data[fac]['latitude']) + "," + str(
+                    city_data[fac]['longitude']) + "&waypoint1=" + str(city_data[cus]['latitude']) + "," + str(
+                    city_data[cus]['longitude']) + "&mode=fastest;car" + cost_opt
+                r = requests.get(request_string)
+
+                print(r.status_code)
+                # print(r.text)
+                here_data = r.json()
+                try:
+                    here_cost = float(here_data['costs']['totalCost'])
+                except:
+                    here_cost = "NA"
+                fact.append(here_cost)
+            cost.append(fact)
+            print(fact)
+
+        with open(cached_file_name, 'wb') as cost_file:
+            pickle.dump(cost, cost_file)
+
+    else:
+        with open(cached_file_name, 'r') as cost_file:
+            cost = pickle.load(cost_file)
 
     print(cost)
 
@@ -55,21 +67,11 @@ def main():
                              pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
     start = time.time()
-    cost_2 = [[90, 76, 75, 70, 50, 74, 12, 68],
-              [35, 85, 55, 65, 48, 101, 70, 83],
-              [125, 95, 90, 105, 59, 120, 36, 73],
-              [45, 110, 95, 115, 104, 83, 37, 71],
-              [60, 105, 80, 75, 59, 62, 93, 88],
-              [45, 65, 110, 95, 47, 31, 81, 34],
-              [38, 51, 107, 41, 69, 99, 115, 48],
-              [47, 85, 57, 71, 92, 77, 109, 36],
-              [39, 63, 97, 49, 118, 56, 92, 61],
-              [47, 101, 71, 60, 88, 109, 52, 90]]
 
     task_sizes = [10, 7, 3, 12, 15, 4, 11, 5]
 
     # Maximum total of task sizes for any worker
-    total_size_max = 15
+    total_size_max = [100, 15, 15, 15, 100]
     num_workers = len(cost)
     num_tasks = len(cost[1])
     # Variables
@@ -83,7 +85,7 @@ def main():
     # The total size of the tasks each worker takes on is at most total_size_max.
 
     for i in range(num_workers):
-        solver.Add(solver.Sum([task_sizes[j] * x[i, j] for j in range(num_tasks)]) <= total_size_max)
+        solver.Add(solver.Sum([task_sizes[j] * x[i, j] for j in range(num_tasks)]) <= total_size_max[i])
 
     # Each task is assigned to at least one worker.
 
